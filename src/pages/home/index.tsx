5e85765079e098d0de7a31c7b10e4cabd74dafa5
Felipe Souza Rasnheski <rasnheskisouzafelipe@gmail.com>
2022-10-27 20:18:05 -0400

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,40 +6,49 @@ import { HomeContainer } from './styles'
 
 export function Home() {
   const [issues, setIssues] = useState<any>({})
+  const [usersFollowing, setUsersFollowing] = useState([])
+
+  function getUsersFollowingRepositories() {
+    const usersInfo = usersFollowing.map(async (user) => {
+      const response = await axios.get(
+        `https://api.github.com/users/${user}/repos`,
+      )
+
+      return response.data
+    })
+  }
+
+  const test = useCallback(async (usersFollowing: string[]) => {
+    const index = Math.ceil(Math.random() * usersFollowing.length)
+
+    const response = await axios.get(
+      `https://api.github.com/users/${usersFollowing[index]}/repos`,
+    )
+
+    const index2 = Math.ceil(Math.random() * response.data.length)
+
+    const { name: repoName, has_issues: hasIssues } = response.data[index2]
+
+    if (hasIssues) {
+      const response2 = await axios.get(
+        `https://api.github.com/repos/${usersFollowing[index]}/${repoName}/issues`,
+      )
+
+      const index3 = Math.ceil(Math.random() * response2.data.length)
+
+      setIssues(response2.data[index3])
+    }
+  }, [])
 
   const fetchFollowing = useCallback(async () => {
     const response = await axios.get(
       'https://api.github.com/users/Felipe-Rasnheski/following',
     )
 
-    async function test(tes: any) {
-      const index = Math.ceil(Math.random() * tes.length)
+    const usersFollowing = response.data.map((user: any) => user.login)
 
-      const { login } = tes[index]
-
-      const response = await axios.get(
-        `https://api.github.com/users/${login}/repos`,
-      )
-
-      const index2 = Math.ceil(Math.random() * response.data.length)
-
-      const { name: repoName, has_issues: hasIssues } = response.data[index2]
-
-      if (hasIssues) {
-        const response2 = await axios.get(
-          `https://api.github.com/repos/${login}/${repoName}/issues`,
-        )
-
-        const index3 = Math.ceil(Math.random() * response2.data.length)
-
-        setIssues(response2.data[index3])
-      }
-    }
-
-    test(response.data)
-  }, [])
-
-  console.log(issues)
+    setUsersFollowing(usersFollowing)
+  }, [setUsersFollowing])
 
   useEffect(() => {
     fetchFollowing()
@@ -51,7 +60,7 @@ export function Home() {
         <img src={githubBlog} alt="" />
       </header>
       <div className="feedContainer">
-        <Issues></Issues>
+        <Issues issue={issues}></Issues>
       </div>
     </HomeContainer>
   )
